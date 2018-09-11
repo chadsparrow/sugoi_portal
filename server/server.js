3778f0272a064a548ad1c5a586938e020b18f33c
chadsparrow <csparrow@sugoi.com>
2018-09-11 11:16:36 -0700

    Reset nodemon to legacy

--- server/server.js
+++ server/server.js
@@ -2,15 +2,15 @@ const express = require("express");
 
 // Constants
 const PORT = 5000;
-const HOST = "0.0.0.0";
+const HOST = "localhost";
 
 // App
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Test");
+  res.send("SUGOI Custom Proofs - REST API Server");
 });
 
-app.listen(PORT, HOST, (req, res) => {
+app.listen(PORT, (req, res) => {
   console.log(`REST API running on http://${HOST}:${PORT}`);
 });
