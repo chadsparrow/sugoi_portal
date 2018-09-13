8b66acef8627994306f18fde17dc35a40880bf33
chadsparrow <csparrow@sugoi.com>
2018-09-13 12:10:17 -0700

    fixed compose & env commits

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 const Style = require("./models/Style");
 
