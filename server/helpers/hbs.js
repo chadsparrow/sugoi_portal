45e0cd77c1d47385e163f07fd8af3f75a2ac842c
chadsparrow <csparrow@sugoi.com>
2018-10-11 14:29:45 -0700

    css changes

--- server/helpers/hbs.js
+++ server/helpers/hbs.js
@@ -24,162 +24,94 @@ module.exports = {
   setStatusDiv: function(status) {
     switch (status) {
       case "1. Waiting for Proof":
-        return (
-          "<div class='chip status red white-text' style='font-size: 10px;'>" +
-          status +
-          "</div>"
-        );
+        return "<div class='chip status red white-text'>" + status + "</div>";
         break;
       case "2. Proof Started":
         return (
-          "<div class='chip status light-blue lighten-2' style='font-size: 10px;'>" +
-          status +
-          "</div>"
+          "<div class='chip status light-blue lighten-2'>" + status + "</div>"
         );
         break;
       case "3. Proof - Waiting on Someone else":
         return (
-          "<div class='chip status light-blue lighten-2' style='font-size: 10px;'>" +
-          status +
-          "</div>"
+          "<div class='chip status light-blue lighten-2'>" + status + "</div>"
         );
         break;
       case "4. Proof Ready for QC":
-        return (
-          "<div class='chip status yellow accent-2' style='font-size: 10px;'>" +
-          status +
-          "</div>"
-        );
+        return "<div class='chip status yellow accent-2'>" + status + "</div>";
         break;
       case "5. Proof QC Complete":
-        return (
-          "<div class='chip status yellow accent-4' style='font-size: 10px;'>" +
-          status +
-          "</div>"
-        );
+        return "<div class='chip status yellow accent-4'>" + status + "</div>";
         break;
       case "6. Proof Complete":
-        return (
-          "<div class='chip status green accent-3' style='font-size: 10px;'>" +
-          status +
-          "</div>"
-        );
+        return "<div class='chip status green accent-3'>" + status + "</div>";
         break;
       case "7. Waiting for Revision":
-        return (
-          "<div class='chip status red white-text' style='font-size: 10px;'>" +
-          status +
-          "</div>"
-        );
+        return "<div class='chip status red white-text'>" + status + "</div>";
         break;
       case "8. Revision - Waiting on Someone else":
         return (
-          "<div class='chip status light-blue lighten-2' style='font-size: 10px;'>" +
-          status +
-          "</div>"
+          "<div class='chip status light-blue lighten-2'>" + status + "</div>"
         );
         break;
       case "9. Revision Started":
         return (
-          "<div class='chip status light-blue lighten-2' style='font-size: 10px;'>" +
-          status +
-          "</div>"
+          "<div class='chip status light-blue lighten-2'>" + status + "</div>"
         );
         break;
       case "10. Revision Ready for QC":
-        return (
-          "<div class='chip status yellow accent-2' style='font-size: 10px;'>" +
-          status +
-          "</div>"
-        );
+        return "<div class='chip status yellow accent-2'>" + status + "</div>";
         break;
       case "11. Revision QC Complete":
-        return (
-          "<div class='chip status yellow accent-4' style='font-size: 10px;'>" +
-          status +
-          "</div>"
-        );
+        return "<div class='chip status yellow accent-4'>" + status + "</div>";
         break;
       case "12. Revision Complete":
-        return (
-          "<div class='chip status green accent-3' style='font-size: 10px;'>" +
-          status +
-          "</div>"
-        );
+        return "<div class='chip status green accent-3'>" + status + "</div>";
         break;
       case "13. Waiting for Output":
-        return (
-          "<div class='chip status red white-text' style='font-size: 10px;'>" +
-          status +
-          "</div>"
-        );
+        return "<div class='chip status red white-text'>" + status + "</div>";
         break;
       case "14. Output - Waiting on Someone else":
         return (
-          "<div class='chip status light-blue lighten-2' style='font-size: 10px;'>" +
-          status +
-          "</div>"
+          "<div class='chip status light-blue lighten-2'>" + status + "</div>"
         );
         break;
       case "15. Output Started":
         return (
-          "<div class='chip status light-blue lighten-2' style='font-size: 10px;'>" +
-          status +
-          "</div>"
+          "<div class='chip status light-blue lighten-2'>" + status + "</div>"
         );
         break;
       case "16. Output Ready for QC":
-        return (
-          "<div class='chip status yellow accent-2' style='font-size: 10px;'>" +
-          status +
-          "</div>"
-        );
+        return "<div class='chip status yellow accent-2'>" + status + "</div>";
         break;
       case "17. Output QC Complete":
-        return (
-          "<div class='chip status yellow accent-4' style='font-size: 10px;'>" +
-          status +
-          "</div>"
-        );
+        return "<div class='chip status yellow accent-4'>" + status + "</div>";
         break;
       case "18. Waiting for PNT":
-        return (
-          "<div class='chip status red white-text' style='font-size: 10px;'>" +
-          status +
-          "</div>"
-        );
+        return "<div class='chip status red white-text'>" + status + "</div>";
         break;
       case "19. PNT Ready for QC":
-        return (
-          "<div class='chip status yellow accent-2' style='font-size: 10px;'>" +
-          status +
-          "</div>"
-        );
+        return "<div class='chip status yellow accent-2'>" + status + "</div>";
         break;
       case "20. PNT QC Complete":
-        return (
-          "<div class='chip status yellow accent-4' style='font-size: 10px;'>" +
-          status +
-          "</div>"
-        );
+        return "<div class='chip status yellow accent-4'>" + status + "</div>";
         break;
       case "21. Uploaded":
         return (
-          "<div class='chip status green darken-2 white-text' style='font-size: 10px;'>" +
+          "<div class='chip status green darken-2 white-text'>" +
           status +
           "</div>"
         );
         break;
       case "22. Sent to Vendor":
         return (
-          "<div class='chip status light-blue darken-4 white-text' style='font-size: 10px;'>" +
+          "<div class='chip status light-blue darken-4 white-text'>" +
           status +
           "</div>"
         );
         break;
       case "23. CANCELLED":
         return (
-          "<div class='chip status grey lighten-2 grey-text' style='font-size: 10px;'>" +
+          "<div class='chip status grey lighten-2 grey-text'>" +
           status +
           "</div>"
         );
