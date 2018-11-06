c09e36e4947aa9dacda3710cdd0d30dc529576cb
chadsparrow <csparrow@sugoi.com>
2018-11-06 11:46:02 -0800

    fixed timestamp in logs

--- server/helpers/logs.js
+++ server/helpers/logs.js
@@ -10,11 +10,16 @@ if (!fs.existsSync(logDir)) {
 winston.remove(winston.transports.Console);
 
 const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: "MM-DD-YYYY HH:mm:ss"
+    }),
+    winston.format.simple()
+  ),
   transports: [
     new winston.transports.File({
       filename: path.join(logDir, "logfile.log"),
-      format: winston.format.simple(),
-      timestamp: true
+      colorize: true
     })
   ]
 });
@@ -22,7 +27,7 @@ const logger = winston.createLogger({
 if (process.env.NODE_ENV !== "production") {
   logger.add(
     new winston.transports.Console({
-      timestamp: true
+      colorize: true
     })
   );
 }
