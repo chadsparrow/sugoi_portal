609d4844c1fc03395190fdabd36c2a9ae2955bd0
chadsparrow <csparrow@sugoi.com>
2018-11-16 09:01:51 -0800

    agenda testing for fedex

--- server/server.js
+++ server/server.js
@@ -73,7 +73,7 @@ mongoose.set("useFindAndModify", false);
 
 // agenda connection
 const agenda = new Agenda({ db: { address: process.env.AGENDA_DB_HOST } });
-agenda.define("send console message", (job, done) => {
+agenda.create("send console message", (job, done) => {
   console.log("Agenda Test " + Date.now());
 })
 
