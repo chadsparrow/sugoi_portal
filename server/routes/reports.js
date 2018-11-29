26cefc31381d461525a9e8a5ec82ec8a26c6e2ca
chadsparrow <csparrow@sugoi.com>
2018-11-29 10:03:49 -0800

    promises for report updates

--- server/routes/reports.js
+++ server/routes/reports.js
@@ -5,41 +5,9 @@ const { ensureAuthenticated } = require("../helpers/auth");
 const Report = require("../models/Report");
 
 router.get("/", ensureAuthenticated, (req, res) => {
-  let averages;
-  Report.aggregate([
-    {
-      $project: {
-        proofAvg: {
-          $trunc: [
-            {
-              $avg: "$proofTurnArounds"
-            }
-          ]
-        },
-        revisionAvg: {
-          $trunc: [
-            {
-              $avg: "$revisionTurnArounds"
-            }
-          ]
-        },
-        outputAvg: {
-          $trunc: [
-            {
-              $avg: "$outputTurnArounds"
-            }
-          ]
-        }
-      }
-    }
-  ]).then(function(res) {
-    averages = res;
-  });
-
   Report.find().then(reports => {
     res.render("reports/index", {
-      reports,
-      averages
+      reports
     });
   });
 });
