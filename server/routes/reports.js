60f37e3bd03209bd33154f8e488ab60c9c410d4f
chadsparrow <csparrow@sugoi.com>
2018-11-29 11:20:37 -0800

    promises for report updates

--- server/routes/reports.js
+++ server/routes/reports.js
@@ -9,26 +9,30 @@ router.get("/", ensureAuthenticated, (req, res) => {
   const currentWeekNum = moment()
     .tz("America/Vancouver")
     .format("W");
-  Report.find().then(reports => {
-    Report.findOne({ reportWeekNumber: currentWeekNum }).then(report => {
-      res.render("reports/index", {
-        reports,
-        report
+  Report.find({})
+    .sort({ reportWeekNumber: -1 })
+    .then(reports => {
+      Report.findOne({ reportWeekNumber: currentWeekNum }).then(report => {
+        res.render("reports/index", {
+          reports,
+          report
+        });
       });
     });
-  });
 });
 
 router.get("/week/:weekNum", ensureAuthenticated, (req, res) => {
   const weekNum = req.params.weekNum;
-  Report.find().then(reports => {
-    Report.findOne({ reportWeekNumber: weekNum }).then(report => {
-      res.render("reports/index", {
-        reports,
-        report
+  Report.find()
+    .sort({ reportWeekNumber: -1 })
+    .then(reports => {
+      Report.findOne({ reportWeekNumber: weekNum }).then(report => {
+        res.render("reports/index", {
+          reports,
+          report
+        });
       });
     });
-  });
 });
 
 module.exports = router;
