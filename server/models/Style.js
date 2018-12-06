1e0b7cd0f3fe21df46c934648339a839a8f87914
chadsparrow <csparrow@sugoi.com>
2018-12-06 07:57:33 -0800

    added thread and changed style models

--- server/models/Style.js
+++ server/models/Style.js
@@ -24,35 +24,36 @@ const StyleSchema = new Schema({
       },
       fabric: {
         type: String
-      }
+      },
+      sizeRange: {
+        type: String
+      },
+      usd1: Number,
+      usd6: Number,
+      usd12: Number,
+      usd24: Number,
+      usd50: Number,
+      usd100: Number,
+      usd200: Number,
+      usd500: Number,
+      cad1: Number,
+      cad6: Number,
+      cad12: Number,
+      cad24: Number,
+      cad50: Number,
+      cad100: Number,
+      cad200: Number,
+      cad500: Number,
+      minUnits: Number,
+      usaTariff: String,
+      cadTariff: String
     }
   ],
   zipperOptions: [String],
   contrastOptions: [String],
-  sizeRange: String,
-  quickDesignOptions: [String],
+  quickDesign: Boolean,
   combinableWith: [String],
-  fit: String,
-  factory: String,
-  usaTariff: String,
-  cadTariff: String,
-  minUnits: Number,
-  usd1: Number,
-  usd6: Number,
-  usd12: Number,
-  usd24: Number,
-  usd50: Number,
-  usd100: Number,
-  usd200: Number,
-  usd500: Number,
-  cad1: Number,
-  cad6: Number,
-  cad12: Number,
-  cad24: Number,
-  cad50: Number,
-  cad100: Number,
-  cad200: Number,
-  cad500: Number
+  factory: String
 });
 
 module.exports = mongoose.model("styles", StyleSchema);
---
+++ server/models/Thread.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ThreadSchema = new Schema({
+  name: {
+    type: String
+  },
+  code:{
+    type: String
+  }
+});
+
+module.exports = mongoose.model("threads", ThreadSchema);
