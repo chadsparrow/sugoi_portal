7a736ee088aecdc932c9311ce67c78ff5c668386
chadsparrow <csparrow@sugoi.com>
2019-01-02 09:35:19 -0800

    set server to http until certs are fixed

--- server/server.js
+++ server/server.js
@@ -14,9 +14,10 @@ const fs = require("fs");
 const tracker = require("delivery-tracker");
 const courier = tracker.courier(tracker.COURIER.FEDEX.CODE);
 const cron = require("cron");
-const privateKey = fs.readFileSync("./certs/star_sugoi_com.key", "utf8");
-const certificate = fs.readFileSync("./certs/star_sugoi_com.crt", "utf8");
-const credentials = { key: privateKey, cert: certificate };
+// un comment these for Secure https fix
+//const privateKey = fs.readFileSync("./certs/star_sugoi_com.key", "utf8");
+//const certificate = fs.readFileSync("./certs/star_sugoi_com.crt", "utf8");
+//const credentials = { key: privateKey, cert: certificate };
 const express = require("express");
 const logger = require("./helpers/logs");
 const moment = require("moment-timezone");
@@ -241,12 +242,16 @@ app.use((error, req, res, next) => {
   res.render("error", { error });
 });
 
-const siteURL = "https://localhost";
 const port = process.env.APP_PORT || 3000;
-// sets https server with certificates and keys
-const httpsServer = https.createServer(credentials, app);
 
-// start the server and listen for requests
-httpsServer.listen(port, (req, res) => {
-  logger.info(`App listening on port ${port} - Go to ${siteURL}:${port}/`);
-});
+// sets https server with certificates and keys
+// un-comment when going live
+// const httpsServer = https.createServer(credentials, app);
+
+// start the https server and listen for requests
+
+// httpsServer.listen(port, (req, res) => {
+//   logger.info(`App listening on port ${port}`);
+// });
+
+app.listen(PORT, logger.info(`App listening on port ${port}`));
