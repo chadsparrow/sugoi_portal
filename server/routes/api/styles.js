26dffc4f2ff6bf258de289d03ee9e2548c73d913
chadsparrow <csparrow@sugoi.com>
2019-01-29 12:17:17 -0800

    develop changes before switch to mac

--- server/routes/api/styles.js
+++ server/routes/api/styles.js
@@ -2,16 +2,14 @@ const express = require("express");
 const router = express.Router();
 const logger = require("../../helpers/logs");
 
-const { ensureAuthenticated, ensureEditOrders } = require("../../helpers/auth");
-
 // includes model for mongodb
-const Order = require("../../models/Order");
+const Style = require("../../models/Style");
 
 // @DESC - GETS JSON DATA OF CERTAIN ORDER NUMBER
 // SEC - MUST BE LOGGED IN
-router.get("/:orderNum", (req, res) => {
-  Order.find({ orderNum: req.params.orderNum })
-    .then(order => {
+router.get("/", (req, res) => {
+  Style.find()
+    .then(styles => {
       res.json(order);
     })
     .catch(err => {
