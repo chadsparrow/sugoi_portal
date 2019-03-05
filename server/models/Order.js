17c8503fd3c3dec9533dc7bea1d34d60f008ca54
Chad Sparrow <csparrow@sugoi.com>
2019-03-05 11:55:58 -0800

    fix date issue

--- server/models/Order.js
+++ server/models/Order.js
@@ -23,7 +23,7 @@ const OrderSchema = new Schema({
   },
   enteredDate: {
     type: Date,
-    default: moment(Date.now).format()
+    default: moment(Date.now()).format()
   },
   eventDate: {
     type: Date,
@@ -46,7 +46,7 @@ const OrderSchema = new Schema({
     {
       date: {
         type: Date,
-        default: moment(Date.now).format()
+        default: moment(Date.now()).format()
       },
       instruction: {
         type: String,
