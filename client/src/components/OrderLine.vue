c7a14f5cd859fb0e875f6145f6ead6efed99a565
Chad Sparrow <csparrow@sugoi.com>
2019-03-05 12:52:44 -0800

    added hrs

--- client/src/components/OrderLine.vue
+++ client/src/components/OrderLine.vue
@@ -1,97 +1,100 @@
 <template>
-  <div class="card border-secondary mb-3" v-if="!orderLine.cancelled">
-    <div class="card-header bg-secondary text-light p-1 justify-items-center">
-      <span>Line: {{orderLine.lineNumber}}</span>
-      <div
-        class="badge badge-warning text-center ml-3"
-        v-if="orderLine.graphicCode != 'CUSTM' && orderLine.graphicCode != null"
-      >Quick Design - 10% OFF</div>
-      <span class="float-right">Details</span>
-    </div>
-    <div class="card-body m-0 p-1">
-      <div class="row m-0 mb-3">
-        <div class="col">
-          Job Type:
-          <span>{{orderLine.lineJobType}}</span>
+  <div v-if="!orderLine.cancelled">
+    <hr>
+    <div class="card border-secondary mb-3">
+      <div class="card-header bg-secondary text-light p-1 justify-items-center">
+        <span>Line: {{orderLine.lineNumber}}</span>
+        <div
+          class="badge badge-warning text-center ml-3"
+          v-if="orderLine.graphicCode != 'CUSTM' && orderLine.graphicCode != null"
+        >Quick Design - 10% OFF</div>
+        <span class="float-right">Details</span>
+      </div>
+      <div class="card-body m-0 p-1">
+        <div class="row m-0 mb-3">
+          <div class="col">
+            Job Type:
+            <span>{{orderLine.lineJobType}}</span>
+          </div>
+          <div class="col" v-if="orderLine.swoReference">
+            SO.Ref#:
+            <span>{{orderLine.swoReference}}</span>
+          </div>
+          <div class="col">
+            Price Break:
+            <span>{{orderLine.priceBreak}} units</span>
+          </div>
+          <div class="col">
+            Graphic:
+            <span>{{orderLine.graphicCode}}</span>
+          </div>
+          <div class="col" v-if="orderLine.colourWayCode">
+            ColourWay:
+            <span>{{orderLine.colourWayCode}}</span>
+          </div>
+          <div class="my-2 col-sm-12 bg-dark text-white text-center rounded">Line Add-Ons</div>
+          <div v-if="orderLine.tracingCharge" class="col-sm-3 text-center">
+            <span
+              class="badge badge-dark text-light"
+            >Tracing: ${{formatPrice(orderLine.tracingCharge)}}</span>
+          </div>
+          <div v-if="orderLine.creativeCharge" class="col-sm-3 text-center">
+            <span
+              class="badge badge-dark text-light"
+            >Creative: ${{formatPrice(orderLine.creativeCharge)}}</span>
+          </div>
+          <div v-if="orderLine.scaledArtCharge" class="col-sm-3 text-center">
+            <span
+              class="badge badge-dark text-light"
+            >Scaled Art: ${{formatPrice(orderLine.scaledArtCharge)}}</span>
+          </div>
+          <div v-if="orderLine.colourWashCharge" class="col-sm-3 text-center">
+            <span
+              class="badge badge-dark text-light"
+            >Colour Wash: ${{formatPrice(orderLine.colourWashCharge)}}</span>
+          </div>
         </div>
-        <div class="col" v-if="orderLine.swoReference">
-          SO.Ref#:
-          <span>{{orderLine.swoReference}}</span>
+        <div class="text-center mt-2">
+          <button
+            type="button"
+            class="btn btn-sm btn-success d-print-none"
+            @click.prevent="goToEdit"
+          >Edit Line Details</button>
         </div>
-        <div class="col">
-          Price Break:
-          <span>{{orderLine.priceBreak}} units</span>
-        </div>
-        <div class="col">
-          Graphic:
-          <span>{{orderLine.graphicCode}}</span>
-        </div>
-        <div class="col" v-if="orderLine.colourWayCode">
-          ColourWay:
-          <span>{{orderLine.colourWayCode}}</span>
-        </div>
-        <div class="my-2 col-sm-12 bg-dark text-white text-center rounded">Line Add-Ons</div>
-        <div v-if="orderLine.tracingCharge" class="col-sm-3 text-center">
-          <span
-            class="badge badge-dark text-light"
-          >Tracing: ${{formatPrice(orderLine.tracingCharge)}}</span>
-        </div>
-        <div v-if="orderLine.creativeCharge" class="col-sm-3 text-center">
-          <span
-            class="badge badge-dark text-light"
-          >Creative: ${{formatPrice(orderLine.creativeCharge)}}</span>
-        </div>
-        <div v-if="orderLine.scaledArtCharge" class="col-sm-3 text-center">
-          <span
-            class="badge badge-dark text-light"
-          >Scaled Art: ${{formatPrice(orderLine.scaledArtCharge)}}</span>
-        </div>
-        <div v-if="orderLine.colourWashCharge" class="col-sm-3 text-center">
-          <span
-            class="badge badge-dark text-light"
-          >Colour Wash: ${{formatPrice(orderLine.colourWashCharge)}}</span>
+        <div class="items mt-2" v-if="orderLine.items.length">
+          <LineItem
+            v-for="(lineitem, childIndex) in orderLine.items"
+            :key="childIndex"
+            :index="childIndex"
+            :lineIndex="index"
+          ></LineItem>
         </div>
       </div>
-      <div class="text-center mt-2">
+      <div class="card-footer p-1">
         <button
           type="button"
-          class="btn btn-sm btn-success d-print-none"
-          @click.prevent="goToEdit"
-        >Edit Line Details</button>
-      </div>
-      <div class="items mt-2" v-if="orderLine.items.length">
-        <LineItem
-          v-for="(lineitem, childIndex) in orderLine.items"
-          :key="childIndex"
-          :index="childIndex"
-          :lineIndex="index"
-        ></LineItem>
-      </div>
-    </div>
-    <div class="card-footer p-1">
-      <button
-        type="button"
-        class="btn btn-sm btn-info mr-1 d-print-none"
-        @click.prevent="addItem"
-      >Add Item</button>
-      <button
-        type="button"
-        class="btn btn-sm btn-danger mr-1 d-print-none"
-        @click.prevent="cancelLine"
-      >Cancel Full Line</button>
-      <div class="float-right">
-        <div
-          class="text-center p-1"
-          v-if="orderLine.totalAddOns > 0"
-        >Sub Total: ${{formatPrice(totalBeforeAddOns)}}</div>
-        <div
-          class="rounded bg-dark text-center text-white p-1"
-          v-if="orderLine.totalAddOns > 0"
-        >Total Add-Ons: ${{formatPrice(orderLine.totalAddOns)}}</div>
-        <div
-          class="rounded bg-secondary text-center text-light p-2"
-          style="font-size: 16px; font-weight: bold;"
-        >Line Total: $ {{formatPrice(orderLine.itemsSubTotal)}}</div>
+          class="btn btn-sm btn-info mr-1 d-print-none"
+          @click.prevent="addItem"
+        >Add Item</button>
+        <button
+          type="button"
+          class="btn btn-sm btn-danger mr-1 d-print-none"
+          @click.prevent="cancelLine"
+        >Cancel Full Line</button>
+        <div class="float-right">
+          <div
+            class="text-center p-1"
+            v-if="orderLine.totalAddOns > 0"
+          >Sub Total: ${{formatPrice(totalBeforeAddOns)}}</div>
+          <div
+            class="rounded bg-dark text-center text-white p-1"
+            v-if="orderLine.totalAddOns > 0"
+          >Total Add-Ons: ${{formatPrice(orderLine.totalAddOns)}}</div>
+          <div
+            class="rounded bg-secondary text-center text-light p-2"
+            style="font-size: 16px; font-weight: bold;"
+          >Line Total: $ {{formatPrice(orderLine.itemsSubTotal)}}</div>
+        </div>
       </div>
     </div>
   </div>
