45f76a3ff94e7c2c292ceca27e48d99769d63f4c
Chad Sparrow <csparrow@sugoi.com>
2019-03-08 10:28:10 -0800

    set comments for staging/production

--- client/vue.config.js
+++ client/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'https://localhost:5000/',
+                target: 'https://localhost:5000',
                 ws: true,
                 changeOrigin: true
             }
