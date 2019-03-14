a86770871111dfb076135d911881c82d703a9d00
Chad Sparrow <csparrow@sugoi.com>
2019-03-14 15:28:56 -0700

    fixed route to cancelled

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,14 +2,11 @@ import Home from './routes/Home.vue'
 import EditDetails from './routes/EditDetails.vue'
 import EditLine from './routes/EditLine.vue'
 import EditItem from './routes/EditItem.vue'
-import Cancelled from './components/Cancelled.vue'
+import Cancelled from './routes/Cancelled.vue'
 
 export default [
-    {
-        path: '/:orderNum', component: Home, children: [
-            { path: '/cancelled', component: Cancelled }
-        ]
-    },
+    { path: '/:orderNum', component: Home },
+    { path: '/:orderNum/cancelled', component: Cancelled },
     { path: '/:orderNum/editdetails', component: EditDetails },
     { path: '/editline/:lineIndex', component: EditLine },
     { path: '/edititem/:lineIndex/:itemIndex', component: EditItem }
--- client/src/routes/Cancelled.vue
+++ client/src/routes/Cancelled.vue
@@ -15,6 +15,7 @@
 
 <script>
 export default {
+  name: "Cancelled",
   computed: {
     cancelledItems() {
       return this.$store.getters.cancelledItems;
