6dc3f902d4ff022398e79139f9ab241831cd248a
Chad Sparrow <csparrow@sugoi.com>
2019-03-14 10:22:33 -0700

    reverted children routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,14 +4,8 @@ import EditLine from './routes/EditLine.vue'
 import EditItem from './routes/EditItem.vue'
 
 export default [
-    {
-        path: '/:orderNum', component: Home, children: [
-            { path: '/editdetails', component: EditDetails },
-        ]
-    },
-    {
-        path: '/editline/:lineIndex', component: EditLine, children: [
-            { path: '/:itemIndex', component: EditItem }
-        ]
-    }
+    { path: '/:orderNum', component: Home },
+    { path: '/:orderNum/editdetails', component: EditDetails },
+    { path: '/editline/:lineIndex', component: EditLine },
+    { path: '/edititem/:lineIndex/:itemIndex', component: EditItem }
 ]
