2663111de9037a5d6de5ef986fa525dbbd5cdfd7
Chad Sparrow <csparrow@sugoi.com>
2019-03-20 13:31:20 -0700

    added totals bottom of OC

--- client/src/routes/Home.vue
+++ client/src/routes/Home.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="home">
-    <order-details></order-details>
-    <order-lines-holder></order-lines-holder>
+    <order-details/>
+    <order-lines-holder/>
+    <totals-box/>
   </div>
 </template>
 
 <script>
 import OrderDetails from "../components/OrderDetails.vue";
 import OrderLinesHolder from "../components/OrderLinesHolder.vue";
+import TotalsBox from "../components/TotalsBox.vue";
 
 export default {
   name: "Home",
   components: {
     OrderDetails,
-    OrderLinesHolder
+    OrderLinesHolder,
+    TotalsBox
   }
 };
 </script>
