e6d53d1d45552e9d02b110c7ee973e131ff0da95
chadsparrow <csparrow@sugoi.com>
2019-05-07 15:53:46 -0700

    fixed oc req limits

--- server/server.js
+++ server/server.js
@@ -129,7 +129,7 @@ require("./config/passport")(passport);
 
 // Body Parser middleware
 app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
 // Method Override
 app.use(methodOverride("_method"));
