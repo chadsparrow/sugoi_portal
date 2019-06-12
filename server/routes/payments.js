b355999c58dad885032fabb9a1b39fc148bef617
chadsparrow <csparrow@sugoi.com>
2019-06-12 10:09:50 -0700

    payments changes to async/await method

--- server/routes/payments.js
+++ server/routes/payments.js
@@ -8,120 +8,123 @@ const { ensureAuthenticated, ensureEditOrders } = require('../helpers/auth');
 // includes model for mongodb
 const Order = require('../models/Order');
 
-router.get('/', [ensureAuthenticated, ensureEditOrders], (req, res) => {
-  let pageTitle = 'Payments - All';
-  Order.find({ currentStatus: { $nin: ['W. CANCELLED', 'X. Archived', '1. Initial'] } }).then(orders => {
-    res.render('payments/', {
-      orders,
-      pageTitle
-    });
-  });
+router.get('/', [ensureAuthenticated, ensureEditOrders], async (req, res) => {
+  try {
+    let pageTitle = 'Payments - All';
+    const orders = await Order.find({ currentStatus: { $nin: ['W. CANCELLED', 'X. Archived', '1. Initial'] } });
+    res.render('payments/', { orders, pageTitle });
+  } catch (err) {
+    logger.error(err);
+  }
 });
 
 // GETS all orders with a balance outstanding but not Cancelled, Archived or Initial Status
-router.get('/outstanding', [ensureAuthenticated, ensureEditOrders], (req, res) => {
-  let pageTitle = 'Payments - Balance Outstanding';
-  Order.find({
-    currentStatus: { $nin: ['W. CANCELLED', 'X. Archived', '1. Initial'] },
-    paymentStatus: { $in: ['Balance Outstanding', 'Refund Customer'] }
-  }).then(orders => {
-    res.render('payments/', {
-      orders,
-      pageTitle
+router.get('/outstanding', [ensureAuthenticated, ensureEditOrders], async (req, res) => {
+  try {
+    let pageTitle = 'Payments - Balance Outstanding';
+    const orders = await Order.find({
+      currentStatus: { $nin: ['W. CANCELLED', 'X. Archived', '1. Initial'] },
+      paymentStatus: { $in: ['Balance Outstanding', 'Refund Customer'] }
     });
-  });
+    res.render('payments/', { orders, pageTitle });
+  } catch (err) {
+    logger.error(err);
+  }
 });
 
 // GETS all orders with a balance outstanding in Pre-Production Status
-router.get('/outstanding/preprod', [ensureAuthenticated, ensureEditOrders], (req, res) => {
-  let pageTitle = 'Payments - Balance Outstanding - Pre-Production';
-  Order.find({
-    currentStatus: {
-      $in: [
-        'A. Waiting for Proof',
-        'B. Proof Started',
-        'C. Proof - Waiting on Someone else',
-        'D. Proof Ready for QC',
-        'D-1. Proof QC in Progress',
-        'E. Proof QC Complete',
-        'F. Proof Complete',
-        'G. Waiting for Revision',
-        'H. Revision - Waiting on Someone else',
-        'I. Revision Started',
-        'J. Revision Ready for QC',
-        'J-1. Revision QC in Progress',
-        'K. Revision QC Complete',
-        'L. Revision Complete'
-      ]
-    },
-    paymentStatus: { $in: ['Balance Outstanding', 'Refund Customer'] }
-  }).then(orders => {
-    res.render('payments/', {
-      orders,
-      pageTitle
+router.get('/outstanding/preprod', [ensureAuthenticated, ensureEditOrders], async (req, res) => {
+  try {
+    let pageTitle = 'Payments - Balance Outstanding - Pre-Production';
+    const orders = await Order.find({
+      currentStatus: {
+        $in: [
+          'A. Waiting for Proof',
+          'B. Proof Started',
+          'C. Proof - Waiting on Someone else',
+          'D. Proof Ready for QC',
+          'D-1. Proof QC in Progress',
+          'E. Proof QC Complete',
+          'F. Proof Complete',
+          'G. Waiting for Revision',
+          'H. Revision - Waiting on Someone else',
+          'I. Revision Started',
+          'J. Revision Ready for QC',
+          'J-1. Revision QC in Progress',
+          'K. Revision QC Complete',
+          'L. Revision Complete'
+        ]
+      },
+      paymentStatus: { $in: ['Balance Outstanding', 'Refund Customer'] }
     });
-  });
+    res.render('payments/', { orders, pageTitle });
+  } catch (err) {
+    logger.error(err);
+  }
 });
 
 // GETS all orders with a balance outstanding in Production Status and not Shipped
-router.get('/outstanding/prod', [ensureAuthenticated, ensureEditOrders], (req, res) => {
-  let pageTitle = 'Payments - Balance Outstanding - In Production';
-  Order.find({
-    currentStatus: {
-      $in: [
-        'M. Waiting for Output',
-        'N. Output - Waiting on Someone else',
-        'O. Output Started',
-        'P. Output Ready for QC',
-        'P-1. Output QC in Progress',
-        'Q. Output QC Complete',
-        'R. Waiting for PNT',
-        'S. PNT Ready for QC',
-        'S-1. PNT QC in Progress',
-        'T. PNT QC Complete',
-        'U. Uploaded',
-        'V. Sent to Vendor'
-      ]
-    },
-    paymentStatus: { $in: ['Balance Outstanding', 'Refund Customer'] },
-    shipStatus: { $ne: 'Shipped' }
-  }).then(orders => {
-    res.render('payments/', {
-      orders,
-      pageTitle
+router.get('/outstanding/prod', [ensureAuthenticated, ensureEditOrders], async (req, res) => {
+  try {
+    let pageTitle = 'Payments - Balance Outstanding - In Production';
+    const orders = await Order.find({
+      currentStatus: {
+        $in: [
+          'M. Waiting for Output',
+          'N. Output - Waiting on Someone else',
+          'O. Output Started',
+          'P. Output Ready for QC',
+          'P-1. Output QC in Progress',
+          'Q. Output QC Complete',
+          'R. Waiting for PNT',
+          'S. PNT Ready for QC',
+          'S-1. PNT QC in Progress',
+          'T. PNT QC Complete',
+          'U. Uploaded',
+          'V. Sent to Vendor'
+        ]
+      },
+      paymentStatus: { $in: ['Balance Outstanding', 'Refund Customer'] },
+      shipStatus: { $ne: 'Shipped' }
     });
-  });
+    res.render('payments/', { orders, pageTitle });
+  } catch (err) {
+    logger.error(err);
+  }
 });
 
 // GETS all orders with a balance outstanding and shipped
-router.get('/outstanding/shipped', [ensureAuthenticated, ensureEditOrders], (req, res) => {
-  let pageTitle = 'Payments - Balance Outstanding - Shipped';
-  Order.find({ shipStatus: 'Shipped', paymentStatus: { $in: ['Balance Outstanding', 'Refund Customer'] } }).then(orders => {
-    res.render('payments/', {
-      orders,
-      pageTitle
-    });
-  });
+router.get('/outstanding/shipped', [ensureAuthenticated, ensureEditOrders], async (req, res) => {
+  try {
+    let pageTitle = 'Payments - Balance Outstanding - Shipped';
+    const orders = await Order.find({ shipStatus: 'Shipped', paymentStatus: { $in: ['Balance Outstanding', 'Refund Customer'] } });
+    res.render('payments/', { orders, pageTitle });
+  } catch (err) {
+    logger.error(err);
+  }
 });
 
-router.get('/edit/:id', [ensureAuthenticated, ensureEditOrders], (req, res) => {
-  const id = req.params.id;
-  Order.findOne({ _id: id }).then(order => {
-    res.render('payments/edit', {
-      order
-    });
-  });
+router.get('/edit/:id', [ensureAuthenticated, ensureEditOrders], async (req, res) => {
+  try {
+    const order = await Order.findOne({ _id: req.params.id });
+    res.render('payments/edit', { order });
+  } catch (err) {
+    logger.error(err);
+  }
 });
 
-router.get('/logs/:id', [ensureAuthenticated, ensureEditOrders], (req, res) => {
-  Order.findOne({ _id: req.params.id }).then(order => {
+router.get('/logs/:id', [ensureAuthenticated, ensureEditOrders], async (req, res) => {
+  try {
+    const order = await Order.findOne({ _id: req.params.id });
     let logs = [];
     const { paymentNotes, paymentNotesDate } = order;
     if (order.paymentNotesLog) {
       logs = order.paymentNotesLog.reverse();
     }
     res.render('payments/logs', { logs, paymentNotes, paymentNotesDate });
-  });
+  } catch (err) {
+    logger.error(err);
+  }
 });
 
 router.put('/edit/:id', [ensureAuthenticated, ensureEditOrders], (req, res) => {
