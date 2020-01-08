4cbdc2901bc55043765ae0a7fc57c6eba22f0083
Chad Sparrow <csparrow@sugoi.com>
2020-01-08 21:13:53 +0000

    Remove elite styles and climbers jerseys

--- server/routes/api/styles.js
+++ server/routes/api/styles.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const logger = require('../../helpers/logs');
+const logger = require("../../helpers/logs");
 
-const { ensureAuthenticated } = require('../../helpers/auth');
+const { ensureAuthenticated } = require("../../helpers/auth");
 
 // includes model for mongodb
-const Style = require('../../models/Style');
-const LGStyle = require('../../models/LGStyle');
+const Style = require("../../models/Style");
+const LGStyle = require("../../models/LGStyle");
 
 // @DESC - GETS ALL STYLE INFO FROM SERVER
 // SEC - MUST BE LOGGED IN.
-router.get('/', ensureAuthenticated, async (req, res) => {
+router.get("/", ensureAuthenticated, async (req, res) => {
   try {
     if (req.user.lgUser) {
       styles = await LGStyle.find();
     } else {
-      styles = await Style.find();
+      styles = await Style.find({ inactive: { $ne: true } });
     }
     res.json(styles);
   } catch (err) {
@@ -23,7 +23,7 @@ router.get('/', ensureAuthenticated, async (req, res) => {
   }
 });
 
-router.get('/lg', ensureAuthenticated, async (req, res) => {
+router.get("/lg", ensureAuthenticated, async (req, res) => {
   try {
     const lgStyles = await LGStyle.find();
     res.json(lgStyles);
